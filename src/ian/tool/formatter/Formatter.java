7922f8dc78f883c7f5062c5ba3e24a4e57dcb507
ian-qiu <qiuyiyi2006@163.com>
2014-03-19 18:57:56 +0800

    a test commit

--- src/ian/tool/formatter/Formatter.java
+++ src/ian/tool/formatter/Formatter.java
@@ -32,6 +32,7 @@ public class Formatter {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         // TODO code application logic here
         JFrame f = new JFrame();
         f.setSize(800, 500);
